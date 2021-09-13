ee157724a11b3be212e977b663e8c4a1d480a735
WebStep <webtostep@yandex.kz>
2021-09-13 23:53:13 +0500

    add servicesModal

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,8 @@ import { pageScroll, scrollHide } from './pageScroll/pageScroll';
 document.addEventListener("DOMContentLoaded", () => {
     const offer = document.getElementById('offer'),
         overlay = document.querySelector('.overlay'),
-        headerModal = document.querySelector('.header-modal');
+        headerModal = document.querySelector('.header-modal'),
+        servicesModal = document.querySelector('.services-modal');
 
     // вызов функций
     scrollHide('none');
@@ -12,15 +13,27 @@ document.addEventListener("DOMContentLoaded", () => {
     // делигирование
     document.addEventListener('click', event => {
         const target = event.target;
-        // открытие модалки
+        const equality = el => el.trim().toLocaleLowerCase();
         if (target.classList.contains('fancyboxModal')) {
+            // открытие модалки
             event.preventDefault();
-            openModal([overlay, headerModal]);
+            if (equality(target.innerText) === equality('ЗАКАЗАТЬ ЗВОНОК!') ||
+            equality(target.innerText) === equality('УЗНАТЬ СВОЮ СКИДКУ')) {
+                openModal([overlay, headerModal]);
+            } else {
+                openModal([overlay, servicesModal]);
+            }
         }
         //  закрытие модалки
-        if (target.classList.contains('overlay') ||
-        target.classList.contains('header-modal__close')) {
+        if (target.classList.contains('header-modal__close')) {
             closeModal([overlay, headerModal]);
+        } else if (target.classList.contains('services-modal__close')) {
+            closeModal([overlay, servicesModal]);
+        }
+        //  закрытие модалки по оверлею
+        if (target.classList.contains('overlay')) {
+            closeModal([overlay, headerModal]);
+            closeModal([overlay, servicesModal]);
         }
         // скрол to top
         if (target.classList.contains('smooth-scroll__img'))pageScroll();
